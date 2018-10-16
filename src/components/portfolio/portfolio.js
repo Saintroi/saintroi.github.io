2d1e8ed0d1ed5ac0122f77bc242e069fa4dd436e
Andrew Nelson <talonroi21@gmail.com>
2018-10-15 22:23:01 -0500

    Portfolio CSS changes

--- src/components/portfolio/portfolio.js
+++ src/components/portfolio/portfolio.js
@@ -9,6 +9,30 @@ export default class Portfolio extends Component {
           <h1>Check Out Some of My Projects.</h1>
           <div id="portfolio"> {/* sage connector */}
             <div className="card">
+              <h1>Sage Connector </h1>
+              <a><Link to="/projects/sage-connector">Project Write-Up</Link> </a>
+              <p>Python, C#</p>
+              <p>A locally-hosted API that enables retrieval of data from a Sage Accounting Database.</p>
+            </div>{/* sage connector end */}
+            <div className="card">
+              test
+            </div>{/* sage connector end */}
+            <div className="card">
+              test
+            </div>{/* sage connector end */}
+            <div className="card">
+              test
+            </div>{/* sage connector end */}
+            <div className="card">
+              test
+            </div>{/* sage connector end */}
+            <div className="card">
+              test
+            </div>{/* sage connector end */}
+            <div className="card">
+              test
+            </div>{/* sage connector end */}
+            <div className="card">
               test
             </div>{/* sage connector end */}
           </div> {/* portfolio end */}
