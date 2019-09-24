9d48f3adee443d0ce394c09b09a27864ef7fd1f2
Drew Nelson <talonroi21@gmail.com>
2019-09-24 12:57:35 -0500

    Taking site down pending overhaul

--- src/App.js
+++ src/App.js
@@ -3,20 +3,19 @@ import { Route, Switch } from 'react-router-dom';
 import Footer from './components/footer/footer';
 import Home from './Pages/Home';
 import Resume from './Pages/Resume';
-import SageConn from './Pages/SageConn'
-import Event2 from './Pages/Event2'
+import SageConn from './Pages/SageConn';
+import Event2 from './Pages/Event2';
 
 class App extends React.Component {
   render() {
     return (
-      <div className="App">
+      <div className='App'>
         <Switch>
-          <Route exact path="/projects/sageconn" component={SageConn} />
-          <Route exact path="/projects/event2" component={Event2} />
-          <Route path="/resume" component={Resume} />
-          <Route exact path="/" component={Home} />
+          <Route exact path='/projects/sageconn' component={SageConn} />
+          <Route exact path='/projects/event2' component={Event2} />
+          <Route path='/resume' component={Resume} />
+          <Route exact path='/' component={Home} />
         </Switch>
-        <Footer />
       </div>
     );
   }
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -5,11 +5,17 @@ import About from '../components/about/about';
 import Skills from '../components/skills/skills';
 import Portfolio from '../components/portfolio/portfolio';
 
+/*<Header resumeData={resumeData}/>
+<About />
+<Skills />
+<Portfolio />*/
+
 export default () => (
   <div>
-    <Header resumeData={resumeData}/>
-    <About />
-    <Skills />
-    <Portfolio />
+    <h1>SITE DOWN FOR MAINTENANCE</h1>
+    <h2>
+      As I update my resume and start applying to new positions, I'll be
+      updating this site to reflect my new experience and abilities.
+    </h2>
   </div>
 );
